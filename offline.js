8143817ba9bfa29398fd03dda5132c4db2682b61
albypiguzGame <albypiguzgithubgame@gmail.com>
2017-11-18 14:55:17 +0100

    First commit just a template

---
+++ offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1511010766,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/groundterrain-sheet0.png",
+		"images/lbutton-sheet0.png",
+		"images/rbutton-sheet0.png",
+		"images/backgroundsky-sheet0.png",
+		"images/player-sheet0.png",
+		"images/weapon-sheet0.png",
+		"images/weapon-sheet1.png",
+		"images/weapon-sheet2.png",
+		"images/shootbutton-sheet0.png",
+		"images/plasmabullet-sheet0.png",
+		"images/jumpup-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
